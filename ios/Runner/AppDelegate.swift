efd6b1a3d10b965cb7e093b65c86aa1cd9d95850
Vasiliki Konstanti <vasia.konstanti@gmail.com>
2025-04-16 21:39:12 +0200

    change on google maps ID token for safety

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,11 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyB93CeRZkgdD80C9ZywsoB70oLTphSFp8M")  
+    if let apiKey = ProcessInfo.processInfo.environment["GOOGLE_MAPS_API_KEY"] {
+        GMSServices.provideAPIKey(apiKey)
+    } else {
+        fatalError("Google Maps API Key is missing")
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
